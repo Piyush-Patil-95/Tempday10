564af4270e36cda9fe63dad4e114c61222fc70d4
ReddyJaiSaiPreetham <dummy@gmail.com>
2026-03-18 01:40:17 +0000

    day23

--- client/src/app/supplylink/components/supplier/supplier.component.ts
+++ client/src/app/supplylink/components/supplier/supplier.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Supplier } from '../../types/Supplier';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { of } from 'rxjs';
-// import { SupplyLinkService } from '../../services/supplylink.service';
-// import { HttpErrorResponse } from '@angular/common/http';
+import { SupplyLinkService } from '../../services/supplylink.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-supplier',
@@ -18,7 +18,7 @@ export class SupplierComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-  //  private supplyLinkService: SupplyLinkService
+    private supplyLinkService: SupplyLinkService
   ) {}
 
   ngOnInit(): void {
@@ -42,33 +42,33 @@ export class SupplierComponent implements OnInit {
   }
 
   onSubmit(): void {
-    // if (this.supplierForm.valid) {
-    //   this.supplyLinkService.addSupplier(this.supplierForm.value).subscribe({
-    //     next: (response) => {
-    //       this.supplier = response;
-    //       this.successMessage = 'Supplier created successfully';
-    //       this.errorMessage = null;
-    //       this.supplierForm.reset();
-    //     },
-    //    // error: (error) => this.handleError(error)
-    //   });
-    // } else {
-    //   this.errorMessage = 'Please fill out all required fields correctly.';
-    //   this.successMessage = null;
-    // }
+    if (this.supplierForm.valid) {
+      this.supplyLinkService.addSupplier(this.supplierForm.value).subscribe({
+        next: (response) => {
+          this.supplier = response;
+          this.successMessage = 'Supplier created successfully';
+          this.errorMessage = null;
+          this.supplierForm.reset();
+        },
+        error: (error) => this.handleError(error)
+      });
+    } else {
+      this.errorMessage = 'Please fill out all required fields correctly.';
+      this.successMessage = null;
+    }
   }
 
-  // private handleError(error: HttpErrorResponse): void {
-  //   if (error.error instanceof ErrorEvent) {
-  //     this.errorMessage = `Client-side error: ${error.error.message}`;
-  //   } else {
-  //     this.errorMessage = `Server-side error: ${error.status} ${error.message}`;
-  //     if (error.status === 400) {
-  //       this.errorMessage = 'Bad request. Please check your input.';
-  //     }
-  //   }
-  //   this.successMessage = null;
-  //   console.error('An error occurred:', this.errorMessage);
-  // }
+  private handleError(error: HttpErrorResponse): void {
+    if (error.error instanceof ErrorEvent) {
+      this.errorMessage = `Client-side error: ${error.error.message}`;
+    } else {
+      this.errorMessage = `Server-side error: ${error.status} ${error.message}`;
+      if (error.status === 400) {
+        this.errorMessage = 'Bad request. Please check your input.';
+      }
+    }
+    this.successMessage = null;
+    console.error('An error occurred:', this.errorMessage);
+  }
 
 }
--- client/src/app/supplylink/services/supplylink.service.ts
+++ client/src/app/supplylink/services/supplylink.service.ts
@@ -1,85 +1,85 @@
-// import { Injectable } from "@angular/core";
-// import { environment } from "src/environments/environment";
-// import { HttpClient } from "@angular/common/http";
-// import { Observable } from "rxjs";
-// import { Supplier } from "../types/Supplier";
-// import { Warehouse } from "../types/Warehouse";
-// import { Product } from "../types/Product";
+import { Injectable } from "@angular/core";
+import { environment } from "/home/coder/app/client/src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Supplier } from "../types/Supplier";
+import { Warehouse } from "../types/Warehouse";
+import { Product } from "../types/Product";
 
-// @Injectable({
-//   providedIn: "root",
-// })
-// export class SupplyLinkService {
-//   private baseUrl = `${environment.apiUrl}`;
+@Injectable({
+  providedIn: "root",
+})
+export class SupplyLinkService {
+  private baseUrl = `${environment.apiUrl}`;
 
-//   constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-//   addSupplier(supplier: Supplier): Observable<any> {
-//     return this.http.post<any>(`${this.baseUrl}/supplier`, supplier);
-//   }
+  addSupplier(supplier: Supplier): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/supplier`, supplier);
+  }
 
-//   editSupplier(supplier: Supplier): Observable<any> {
-//     return this.http.put<any>(`${this.baseUrl}/supplier/${supplier.supplierId}`, supplier);
-//   }
+  editSupplier(supplier: Supplier): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/supplier/${supplier.supplierId}`, supplier);
+  }
 
-//   deleteSupplier(supplierId: number): Observable<any> {
-//     return this.http.delete(`${this.baseUrl}/supplier/${supplierId}`);
-//   }
+  deleteSupplier(supplierId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/supplier/${supplierId}`);
+  }
 
-//   getSupplierById(supplierId: number): Observable<Supplier> {
-//     return this.http.get<Supplier>(`${this.baseUrl}/supplier/${supplierId}`);
-//   }
+  getSupplierById(supplierId: number): Observable<Supplier> {
+    return this.http.get<Supplier>(`${this.baseUrl}/supplier/${supplierId}`);
+  }
 
-//   getAllSuppliers(): Observable<Supplier[]> {
-//     return this.http.get<Supplier[]>(`${this.baseUrl}/supplier`);
-//   }
+  getAllSuppliers(): Observable<Supplier[]> {
+    return this.http.get<Supplier[]>(`${this.baseUrl}/supplier`);
+  }
 
-//   addWarehouse(warehouse: Warehouse): Observable<any> {
-//     return this.http.post<Warehouse>(`${this.baseUrl}/warehouse`, warehouse);
-//   }
+  addWarehouse(warehouse: Warehouse): Observable<any> {
+    return this.http.post<Warehouse>(`${this.baseUrl}/warehouse`, warehouse);
+  }
 
-//   editWarehouse(warehouse: Warehouse): Observable<any> {
-//     return this.http.put<any>(`${this.baseUrl}/warehouse/${warehouse.warehouseId}`, warehouse);
-//   }
+  editWarehouse(warehouse: Warehouse): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/warehouse/${warehouse.warehouseId}`, warehouse);
+  }
 
-//   deleteWarehouse(warehouseId: number): Observable<any> {
-//     return this.http.delete(`${this.baseUrl}/warehouse/${warehouseId}`);
-//   }
+  deleteWarehouse(warehouseId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/warehouse/${warehouseId}`);
+  }
 
-//   getWarehouseById(warehouseId: number): Observable<Warehouse> {
-//     return this.http.get<Warehouse>(`${this.baseUrl}/warehouse/${warehouseId}`);
-//   }
+  getWarehouseById(warehouseId: number): Observable<Warehouse> {
+    return this.http.get<Warehouse>(`${this.baseUrl}/warehouse/${warehouseId}`);
+  }
 
-//   getAllWarehouses(): Observable<Warehouse[]> {
-//     return this.http.get<Warehouse[]>(`${this.baseUrl}/warehouse`);
-//   }
+  getAllWarehouses(): Observable<Warehouse[]> {
+    return this.http.get<Warehouse[]>(`${this.baseUrl}/warehouse`);
+  }
 
-//   getWarehousesBySupplier(supplierId: number): Observable<Warehouse[]> {
-//     return this.http.get<Warehouse[]>(`${this.baseUrl}/warehouse/supplier/${supplierId}`);
-//   }
+  getWarehousesBySupplier(supplierId: number): Observable<Warehouse[]> {
+    return this.http.get<Warehouse[]>(`${this.baseUrl}/warehouse/supplier/${supplierId}`);
+  }
 
-//   addProduct(product: Product): Observable<any> {
-//     return this.http.post<Product>(`${this.baseUrl}/product`, product);
-//   }
+  addProduct(product: Product): Observable<any> {
+    return this.http.post<Product>(`${this.baseUrl}/product`, product);
+  }
 
-//   editProduct(product: Product): Observable<any> {
-//     return this.http.put<any>(`${this.baseUrl}/product/${product.productId}`, product);
-//   }
+  editProduct(product: Product): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/product/${product.productId}`, product);
+  }
 
-//   deleteProduct(productId: number): Observable<any> {
-//     return this.http.delete(`${this.baseUrl}/product/${productId}`);
-//   }
+  deleteProduct(productId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/product/${productId}`);
+  }
 
-//   getProductById(productId: number): Observable<Product> {
-//     return this.http.get<Product>(`${this.baseUrl}/product/${productId}`);
-//   }
+  getProductById(productId: number): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/product/${productId}`);
+  }
 
-//   getAllProducts(): Observable<Product[]> {
-//     return this.http.get<Product[]>(`${this.baseUrl}/product`);
-//   }
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/product`);
+  }
 
-//   getAllProductByWarehouse(warehouseId: number | null): Observable<Product[]> {
-//     return this.http.get<Product[]>(`${this.baseUrl}/product/warehouse/${warehouseId}`);
-//   }
+  getAllProductByWarehouse(warehouseId: number | null): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/product/warehouse/${warehouseId}`);
+  }
 
-// }
+}
